e7504f70151cc5303128384eb197394cd3eccfee
Craig McKeachie <craigdmckeachie@gmail.com>
2022-01-14 09:54:16 -0500

    Add project files.

---
+++ AutoMapperDemo/Program.cs
@@ -0,0 +1,102 @@
+﻿using AutoMapper;
+using System;
+
+namespace AutoMapperDemo
+{
+
+    class Wedding
+    {
+        public DateTime Date { get; set; }
+
+        public Person Bride { get; set; }
+
+        public Person Groom { get; set; }
+    }
+
+    class Person
+    {
+        public Title Title { get; set; }
+
+        public string Name { get; set; }
+
+        public Address Address { get; set; }
+    }
+
+    class Address
+    {
+        public string Line1 { get; set; }
+    }
+
+    enum Title
+    {
+        Other, Mr, Ms, Miss, Mrs, Dr
+    }
+
+
+    class WeddingViewModel
+    {
+        public DateTime Date { get; set; }
+        //public int Id { get; set; }
+
+        public string BrideTitle { get; set; }
+
+        public string BrideName { get; set; }
+
+        public string BrideAddressLine1 { get; set; }
+
+        public string GroomTitle { get; set; }
+
+        public string GroomName { get; set; }
+
+        public string GroomAddressLine1 { get; set; }
+    }
+
+
+    class Program
+    {
+
+ 
+        static void Main(string[] args)
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Wedding, WeddingViewModel>();
+            });
+         
+
+            try
+            {
+                config.AssertConfigurationIsValid();
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.ToString());
+            }
+
+            Person bride = new Person
+            {
+                Name = "Beyonce",
+                Title = Title.Mrs,
+                Address = new Address { Line1 = "123 Manhattan Ave" }
+            };
+
+            Person groom = new Person
+            {
+                Name = "Sean Carter",
+                Title = Title.Mr,
+                Address = new Address { Line1 = "123 Manhattan Ave" }
+            };
+
+            IMapper mapper = new Mapper(config);
+
+            var wedding = new Wedding { Bride = bride, Groom = groom };
+
+
+            var weddingViewModel = mapper.Map<WeddingViewModel>(wedding);
+
+
+            Console.WriteLine("End");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ EmployeeDemo/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using AutoMapper;
+namespace AutoMapperDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initialize the mapper
+            var config = new MapperConfiguration(cfg =>
+                    cfg.CreateMap<Employee, EmployeeDTO>()
+                );
+
+            //Creating the source object
+            Employee emp = new Employee
+            {
+                Name = "James",
+                Salary = 20000,
+                Address = "London",
+                Department = "IT"
+            };
+
+            //Using automapper
+            IMapper mapper = new Mapper(config);
+            var empDTO = mapper.Map<EmployeeDTO>(emp);
+            //OR
+            //var empDTO2 = mapper.Map<Employee, EmployeeDTO>(emp);
+
+            Console.WriteLine("Name:" + empDTO.Name + ", Salary:" + empDTO.Salary + ", Address:" + empDTO.Address + ", Department:" + empDTO.Department);
+            Console.ReadLine();
+        }
+    }
+
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public string Address { get; set; }
+        public string Department { get; set; }
+    }
+
+    public class EmployeeDTO
+    {
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public string Address { get; set; }
+        public string Department { get; set; }
+    }
+}
